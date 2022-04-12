eda2e6b16bca0555e7ad80c8f1c1d7c4319b557c
Nahid-ul-Islam <nahidul522@gmail.com>
2022-04-13 00:08:43 +0600

    Login page created

---
+++ src/Components/SignUp/SignUp.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './SignUp.css';
+
+const Signup = () => {
+    return (
+        <div>
+            <h2>SignUP</h2>
+        </div>
+    );
+};
+
+export default Signup;
